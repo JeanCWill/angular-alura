264aea21c6fa5bca1a4cfbe7127d748bfad3ac8b
Jean Will <jcarloswill@gmail.com>
2020-01-12 19:57:09 -0300

    Commit do projeto parte 1

---
+++ src/app/photos/photo/photo.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Injectable } from "@angular/core";
+import { Photo } from "./photo";
+
+const API = 'http://localhost:3000';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PhotoService {
+    constructor(private _http: HttpClient) {}
+
+    listFromUser(userName: string): Observable<Photo[]> {
+        return this._http
+            .get<Photo[]>(`${API}/${userName}/photos`);
+    }
+
+    listFromUserPaginated(userName: string, page: number): Observable<Photo[]> {
+        const params = new HttpParams().append('page', page.toString());
+
+        return this._http
+            .get<Photo[]>(`${API}/${userName}/photos`, { params });
+    }
+}
